29d8c327b4822cc854d24275f49c3e882faec703
Dinersa <dinersiakhoza@gmail.com>
2023-11-05 21:20:13 +0200

    added the API key

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+function searchCity(city){
+    let apiKey = "5ff91039bfoa7d3c00e40a4db3ace0et";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#Weather-App-City");
+    let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchForElement = document.querySelector("#search-form");
