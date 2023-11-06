82eb3d4f8fcdd2c8ef88dad2e11ccae9a2436ad3
Dinersa <dinersiakhoza@gmail.com>
2023-11-06 18:21:41 +0200

    added the search engine data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+function refreshWeather(response) {
+    let temparatureElement = document.querySelector("#temparature");
+   let temparature = response.data.temparature.current;
+    temparatureElement.innerHTML = Math.round(temparature);
+}
+
 function searchCity(city){
     let apiKey = "5ff91039bfoa7d3c00e40a4db3ace0et";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-     
+     axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event){
