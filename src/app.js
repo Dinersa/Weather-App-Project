1912ce95bf0198d55f85b21c69f15840d7a2ef4d
Dinersa <dinersiakhoza@gmail.com>
2023-11-06 17:53:05 +0200

    updated api url

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 function searchCity(city){
     let apiKey = "5ff91039bfoa7d3c00e40a4db3ace0et";
-    let apiUrl =
-      'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKeyY}'
-     ;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+     
 }
 
 function handleSearchSubmit(event){
