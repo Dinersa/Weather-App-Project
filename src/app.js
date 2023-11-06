326369e57f666bf9b5d94426d4ea8e2c96fd3848
Dinersa <dinersiakhoza@gmail.com>
2023-11-06 18:46:39 +0200

    Added search engine using API

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 function refreshWeather(response) {
-    let temparatureElement = document.querySelector("#temparature");
-   let temparature = response.data.temparature.current;
-    temparatureElement.innerHTML = Math.round(temparature);
+  let temparatureElement = document.querySelector("#temparature");
+  let temparature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temparatureElement.innerHTML = Math.round(temparature);
 }
 
-function searchCity(city){
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=5ff91039bfoa7d3c00e40a4db3ace0et&units=metric`;
-     axios.get(apiUrl).then(refreshWeather);
+function searchCity(city) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=5ff91039bfoa7d3c00e40a4db3ace0et&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
 
 let searchForElement = document.querySelector("#search-form");
-searchForElement.addEventListener("submit",handleSearchSubmit);
+searchForElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Johannesburg");
